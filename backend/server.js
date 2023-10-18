5ed73279b84fa7b89b472f403ca65aba1f96e4e8
nii_okantey <nunoogideon19@gmail.com>
2023-10-18 13:31:16 +0000

    updates

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+const PORT = process.env.PORT || 8000
+
+
+const app = express()
+app.get("/", (request, response) => {
+  response.status(200).json({
+    message: "Server is up and running!",
+    data: ["Micheal", "Joshua"]
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
