e674f46cd59ef7966f1275d5882fe19b6f108ecb
nii_okantey <nunoogideon19@gmail.com>
2023-10-18 14:24:37 +0000

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes.js"
 dotenv.config()
 const PORT = process.env.PORT || 8000
-
-
 const app = express()
+
+
+app.use("/api/users", userRoutes)
 app.get("/", (request, response) => {
   response.status(200).json({ message: "Server is up and running!" })
 })
