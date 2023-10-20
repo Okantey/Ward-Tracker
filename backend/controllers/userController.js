75c9c5eaa1357c32c0001177f061da8c27deb014
nii_okantey <nunoogideon19@gmail.com>
2023-10-20 12:01:11 +0000

    updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler"
 import User from "../models/userModel.js"
+import generateToken from "../utils/generateToken.js"
 
 
 // @desc auth user / set token
@@ -7,40 +8,64 @@ import User from "../models/userModel.js"
 // @access PUBLIC
 
 const authUser = asyncHandler(async (request, response) => {
-  response.status(200).json({ message: "Auth user" })
-})
+  const { email, password } = request.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(response, user._id);
+
+    response.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    response.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
 // @desc register user
 //route POST api/users/register
 // @access PUBLIC
 
 const registerUser = asyncHandler(async (request, response) => {
-  const { name, email, password } = request.body
-  const userExists = await User.findOne({ email })
+  const { name, email, password } = request.body;
+  const userExists = await User.findOne({ email });
   if (userExists) {
-    response.status(400)
-    throw new Error("User already exists")
+    response.status(400);
+    throw new Error('User already exists');
   }
-  const user = await User.create({ name, email, password })
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
   if (user) {
+    generateToken(response, user._id);
     response.status(201).json({
       _id: user._id,
       name: user.name,
-      email: user.email
-    })
+      email: user.email,
+    });
   } else {
-    response.status(400)
-    throw new Error("Invalid user data")
+    response.status(400);
+    throw new Error('Invalid user data');
   }
-
-})
+});
 
 // @desc logout user
 //route POST api/users/logout
 // @access PUBLIC
 
-const logoutUser = asyncHandler(async (request, response) => {
-  response.status(200).json({ message: "Logout user" })
-})
+const logoutUser = (request, response) => {
+  response.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  response.status(200).json({ message: 'Logged out successfully' });
+};
 
 // @desc get user profile
 //route GET api/users/profile
